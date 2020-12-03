7417155f175831795a398a178c6f3e58c8d75fea
gideonflash <gideonkadzura@gmail.com>
2020-12-03 00:08:31 +0000

    folds

---
+++ 9_folds/folds.hs
@@ -0,0 +1,11 @@
+-- fold -> from some collection to a value
+
+-- foldr
+-- (1 + (2 +(3 + start))
+
+-- foldl
+-- (start + (1 + (2 + 3)))
+
+-- between the two associativity is the difference
+
+-- scanning ->
